b326584822a4066708e6fe15d212d2941b1e6970
burair-ahmed <burair.s3380@gmail.com>
2024-11-25 17:58:47 +0500

    Remove "backgroundColor" and "borderColor" from theme.extend.colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        backgroundColor: ['checked'],
-      borderColor: ['checked'],
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
