5e7d816470ac137ce26192bb392bf8c562ede494
burair-ahmed <burair.s3380@gmail.com>
2024-12-16 18:04:41 +0500

    `Remove req parameter from middleware function`

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware() {
 
 
   return NextResponse.next();
