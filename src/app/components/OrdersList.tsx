ec3be807803db9ceee03b3fd6de26ae1eecdc2ef
burair-ahmed <burair.s3380@gmail.com>
2024-12-16 18:33:41 +0500

    `Removed playNotificationSound function and commented out code`

--- src/app/components/OrdersList.tsx
+++ src/app/components/OrdersList.tsx
@@ -92,22 +92,22 @@ const OrdersList: FC = () => {
   }, [lastFetched]);
 
   // Play notification sound when a new order is added
-  const playNotificationSound = () => {
-    if (audioContextRef.current && audioBufferRef.current) {
-      try {
-        const audioContext = audioContextRef.current;
-        const source = audioContext.createBufferSource();
-        source.buffer = audioBufferRef.current;
+  // const playNotificationSound = () => {
+  //   if (audioContextRef.current && audioBufferRef.current) {
+  //     try {
+  //       const audioContext = audioContextRef.current;
+  //       const source = audioContext.createBufferSource();
+  //       source.buffer = audioBufferRef.current;
 
-        source.connect(audioContext.destination);
-        source.start(0);
+  //       source.connect(audioContext.destination);
+  //       source.start(0);
 
-        console.log("Notification sound played successfully.");
-      } catch (error) {
-        console.error("Error playing notification sound:", error);
-      }
-    }
-  };
+  //       console.log("Notification sound played successfully.");
+  //     } catch (error) {
+  //       console.error("Error playing notification sound:", error);
+  //     }
+  //   }
+  // };
 
   // Update order status when changed
   const updateOrderStatus = async (orderNumber: string, newStatus: string) => {
